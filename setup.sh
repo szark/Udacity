f14a1702004cae7cd119aacf0d79c3d9d4a29687
Lukasz Starsiak <lukasz.starsiak@4data.ch>
2021-08-09 21:31:54 +0200

    add terraform config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-export DEBIAN_FRONTEND=noninteractove
+export DEBIAN_FRONTEND=noninteractive
 apt-get install unzip awscli -y
 apt-get install apache2 -y
 systemctl start apache2.service
